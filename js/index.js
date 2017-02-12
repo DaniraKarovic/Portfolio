43a5ea56b0c45f6b3322b861a7b76a6b01ccf585
Danira Karovic <karovic.danira@gmail.com>
2017-02-12 18:30:50 +0100

    add files

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$("#contact-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 2000);
+});
+
+$("#portfolio-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#portfolio").offset().top
+    }, 2000);
+});
+
+$("#about-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+});
